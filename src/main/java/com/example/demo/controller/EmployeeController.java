54793a503cd853706544ca0522ce053ad21037e2
Saritha-at-289810389200 <saritha.bolla@gmail.com>
2020-04-16 20:31:22 +0530

    sonarQube test

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -23,7 +23,7 @@ public class EmployeeController {
     @GetMapping("/")
     public ResponseEntity<List<Employee>> getEmployees(){
         List<Employee> emp = employeeService.getEmployeeList();
-        System.out.println("Testing");
+        System.out.println("dummy");
         return new ResponseEntity<>(emp, HttpStatus.OK);
     }
 }
