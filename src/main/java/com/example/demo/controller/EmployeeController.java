730ec265b7ced14e880562beeb426cf74b3dd4d2
Saritha-at-289810389200 <saritha.bolla@gmail.com>
2020-04-16 14:39:53 +0530

    Test

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -23,7 +23,6 @@ public class EmployeeController {
     @GetMapping("/")
     public ResponseEntity<List<Employee>> getEmployees(){
         List<Employee> emp = employeeService.getEmployeeList();
-        System.out.println("test");
         return new ResponseEntity<>(emp, HttpStatus.OK);
     }
 }
