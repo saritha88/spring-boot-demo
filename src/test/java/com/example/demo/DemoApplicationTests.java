77ea0271a42ffe8d5e1c480201a0507dfecddfb3
Saritha-at-289810389200 <saritha.bolla@gmail.com>
2020-04-20 15:26:38 +0530

    changes

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -28,12 +28,10 @@ class DemoApplicationTests {
 		assertNotNull(employeeService.getEmployeeList());
 	}
 
-/*
 	@Test
 	public void testEmpList1(){
 		assertNotNull(employeeController.getEmployees());
 	}
-*/
 
 
 }
