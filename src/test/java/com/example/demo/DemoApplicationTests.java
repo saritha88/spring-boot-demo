569f8f2dfee202d0e297e888dad9d998d3b9e227
Saritha-at-289810389200 <saritha.bolla@gmail.com>
2020-04-20 15:08:09 +0530

    Test coverage

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.controller.EmployeeController;
 import com.example.demo.model.Employee;
 import com.example.demo.service.EmployeeService;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
@@ -18,9 +20,18 @@ class DemoApplicationTests {
 	@InjectMocks
 	private EmployeeService employeeService;
 
+	@Autowired
+	private EmployeeController employeeController;
+
 	@Test
 	public void testEmpList(){
 		assertNotNull(employeeService.getEmployeeList());
 	}
+
+	@Test
+	public void testEmpList1(){
+		assertNotNull(employeeController.getEmployees());
+	}
+
 
 }
